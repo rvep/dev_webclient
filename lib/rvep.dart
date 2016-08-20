e000b2c91b26acb92583aa03142f8d4e059ef603
borysn <xborysn@gmail.com>
2016-08-20 18:17:54 -0500

    dashboard route

--- lib/rvep.dart
+++ lib/rvep.dart
@@ -5,13 +5,15 @@ library dev_webclient.lib.rvep;
 
 import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
-import 'package:dev_webclient/views/login/login.dart';
+import 'package:dev_webclient/views/signin/signin.dart';
+import 'package:dev_webclient/views/dashboard/dashboard.dart';
 
 @Component(
   selector: 'rvep',
   template: '<router-outlet></router-outlet>',
   directives: const [ROUTER_DIRECTIVES])
 @RouteConfig(const [
-  const Route(path: '/login', name: 'Lome', component: Login, useAsDefault: true),
+  const Route(path: '/signin', name: 'Signin', component: Signin, useAsDefault: true),
+  const Route(path: '/dashboard', name: 'Dashboard', component: Dashboard)
 ])
 class Rvep {}
