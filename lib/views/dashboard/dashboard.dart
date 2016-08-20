6c9ae5f8c7612734444ce7fd89c744c85306d648
borysn <xborysn@gmail.com>
2016-08-20 18:17:28 -0500

    add dashboard view

---
+++ lib/views/dashboard/dashboard.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2016, borysn. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+library dev_webclient.lib.views.dashboard;
+
+import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
+
+@Component(
+  selector: 'dashboard',
+  styleUrls: const ['dashboard.css'],
+  templateUrl: 'dashboard.html',
+  directives: const [ROUTER_DIRECTIVES])
+class Dashboard {}
