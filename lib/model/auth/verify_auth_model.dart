bdf0bc8a308d4c9db53018c39dcda8af76a26119
borysn <xborysn@gmail.com>
2016-08-21 12:39:23 -0500

    update library declaration

--- lib/model/auth/verify_auth_model.dart
+++ lib/model/auth/verify_auth_model.dart
@@ -1,7 +1,7 @@
 // Copyright (c) 2016, borysn. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-library dev_webclient.lib.model.auth.verifyAuthModel;
+library dev_webclient.lib.model.auth.verify_auth_model;
 
 class VerifyAuthModel {
   bool _isVerified;
